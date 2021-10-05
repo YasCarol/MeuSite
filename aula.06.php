add9bc8c2b4e4c6aff8b05ee98e1d2dd79064f99
YasCarol <carolineyasmin815@gmail.com>
2021-10-04 22:35:28 -0300

    aula6

---
+++ aula.06.php
@@ -0,0 +1,44 @@
+<html>
+<head>
+<meta charset="UTF-8"/>
+<link rel="stylesheet" href="_css/estilo.css"/>
+<title>Aula 06</title>
+</head>
+<body>
+<div>
+<?php
+/*como eu fiz sozinha (EXERCICIO 01, 10% de aumento/deconto, UTILIZANDO OPERADORES DE ATRIBUICAO)
+$preço_total = $_GET['x'];
+$aumento = ($preço_total*10)/100;
+$preço_desconto = $preço_total + $aumento;
+echo " O valor R$ ". number_format($preço_total,2). "<br/> com de 10% de aumento será de R$ ". number_format($preço_desconto,2);
+
+Gustavo
+$preco = $_GET['x'];
+echo "O preço do produto é de R$". number_format($preco,2);
+$preco += $preco*10/100;
+echo "<br/>E o novo preco com 10% de aumento será de R$". number_format($preco,2);
+
+(EXERCICIO 02, qual é o ano anterior, UTILIZANDO OPERADORES DE INCREMENTO)
+$ano_ant = $_GET['a'];
+echo "O ano anterior ao ano $ano_ant é ". --$ano_ant;
+
+(EXERCICIO 03, USO DE VARIAVEIS REFERENCIADAS &)
+$a = 3;
+$b = &$a;
+$b += 5;
+echo "A variavel A vale $a";
+echo "<br/>A variavel B vale $b";
+
+//(EXERCICIO 04, USO DE VARIAVEIS DE VARIAVEIS)
+$x = "abc";
+$$x = "def";
+echo "O conteudo da variavel X é $x";
+echo "<br/>A ABC criada recebeu o valor de $abc";*/
+?>
+
+
+<?= " imprima essa string"?>
+</div>
+</body>
+</html>
